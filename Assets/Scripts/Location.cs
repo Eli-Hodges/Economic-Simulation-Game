0d0f5fda4e5a806ff134a9ee2ea859af5f0da32d
Eli-Hodges <elijah.drew.hodges@gmail.com>
2024-10-28 12:08:28 -0600

    Piecemeal addition part one

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Location : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Console.Log("Location script is running");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
